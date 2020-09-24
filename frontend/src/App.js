d28476ce2a9b004ac591693d1f5739f3f1e0fc59
Hangnhk <13665132@student.uts.edu.au>
2020-09-24 19:37:05 +1000

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
-import Header from './components/Header';
-import Login from './components/Login';
-import Register from './components/Register';
-import Leaderboard from './components/Leaderboard';
-import Home from './components/Home';
+// This is Hailey
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Leaderboard from "./components/Leaderboard";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="app"> 
+    <div className="app">
       <Router>
-      <Header />
-      <div className="app__body">
-        <Switch>
-          <Route path="/leaderboard">
-            <Leaderboard />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          {/* This is the default route */}
+        <Header />
+        <div className="app__body">
+          <Switch>
+            <Route path="/leaderboard">
+              <Leaderboard />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            {/* This is the default route */}
 
-          <Route path="/">  
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </div>
   );
